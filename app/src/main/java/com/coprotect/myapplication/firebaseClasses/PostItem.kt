5f43f280a1d8be528b6acd9ef59430605ba26786
Sanskruti Dholu <83569296+sanskrutidholu@users.noreply.github.com>
2021-09-29 14:18:21 +0530

    post item model class

--- app/src/main/java/com/coprotect/myapplication/firebaseClasses/PostItem.kt
+++ app/src/main/java/com/coprotect/myapplication/firebaseClasses/PostItem.kt
@@ -1,5 +1,62 @@
 package com.coprotect.myapplication.firebaseClasses
 
+import kotlin.properties.Delegates
+
+
+class PostItem {
+    var pTitle: String? = null
+    var pId: String? = null
+    var pDescription: String? = null
+    var pLikes: String? = null
+    var pImage: String? = null
+    var pTime: String? = null
+    var uid: String? = null
+    var uDp: String? = null
+    var uName: String? = null
+    var pComments: String? = null
+
+    constructor() {}
+    constructor(
+        pId: String?,
+        pDescription: String?,
+        pTitle : String?,
+        pLikes: String?,
+        pImage: String?,
+        pTime: String?,
+        uid: String?,
+        uDp: String?,
+        uName: String?,
+        pComments: String?
+    ) {
+        this.pId = pId
+        this.pDescription = pDescription
+        this.pTitle = pTitle
+        this.pLikes = pLikes
+        this.pImage = pImage
+        this.pTime = pTime
+        this.uid = uid
+        this.uDp = uDp
+        this.uName = uName
+        this.pComments = pComments
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*package com.coprotect.myapplication.firebaseClasses
+
 class PostItem(val userId: String,
                val postId: String,
                val postType: String,
@@ -9,4 +66,4 @@ class PostItem(val userId: String,
                val commentCount: String,
                val postTimeInMillis: Long) {
     constructor(): this("", "", "", "", "", -1, "", -1)
-}
+}*/
